de10d56790cce68564acb408e3d45ead15817f95
strangerelics <castlecryptoid@gmail.com>
2017-12-09 12:44:03 -0800

    Fix api call bug

--- lib/tabletalk_web/views/error_view.ex
+++ lib/tabletalk_web/views/error_view.ex
@@ -4,4 +4,8 @@ defmodule TabletalkWeb.ErrorView do
   def render("500.json", error) do
     %{message: "Sorry, something broke on our end."}
   end
+
+  def render("404.json", _err) do
+    %{message: "The requested resource could not be found."}
+  end
 end
