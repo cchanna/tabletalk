46a680d235c5c00b96ecb78f4d4c03cb1ecaeb51
strangerelics <castlecryptoid@gmail.com>
2017-12-08 23:07:21 -0800

    Fix errors

--- build_upgrade.sh
+++ build_upgrade.sh
@@ -5,5 +5,5 @@ MIX_ENV=prod mix do phx.digest, release --upgrade
 export version=$(ls _build/prod/rel/tabletalk/releases -v | tail -n 3 | head -n 1)
 mkdir ../deploy/releases/$version
 cp _build/prod/rel/tabletalk/releases/$version/tabletalk.tar.gz ../deploy/releases/$version
-echo successfully built upgrade version $version
-echo run \"bash upgrade.sh $version\" to upgrade
+echo Successfully built upgrade version $version
+echo Run \"sudo bash upgrade.sh $version\" to upgrade
--- lib/tabletalk_web/views/error_view.ex
+++ lib/tabletalk_web/views/error_view.ex
@@ -2,6 +2,6 @@ defmodule TabletalkWeb.ErrorView do
   use TabletalkWeb, :view
 
   def render("500.json", error) do
-    %{message: error}
+    %{message: "Sorry, something broke on our end."}
   end
 end
