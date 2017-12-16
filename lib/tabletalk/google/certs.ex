bfe7d6a00f7872df7b07c497bde96d18f5d61f57
strangerelics <castlecryptoid@gmail.com>
2017-12-15 16:59:30 -0800

    refresh google certs every hour

--- lib/tabletalk/google/certs.ex
+++ lib/tabletalk/google/certs.ex
@@ -2,20 +2,26 @@ defmodule Tabletalk.Google.Certs do
   alias Tabletalk.Google.API
   use Agent
 
+  require Logger
+
   def start_link do
     Agent.start_link(fn -> nil end, name: __MODULE__)
   end
 
   defp get_from_server do
     result = API.certs |> Enum.map(&(&1))
-    :ok = Agent.update(__MODULE__, fn _state -> result end)
+    :ok = Agent.update(__MODULE__, fn _state -> {result, get_now() + 15} end)
     result
   end
 
+  defp get_now, do: DateTime.to_unix(DateTime.utc_now)
+
   def get do
+    now = get_now()
     case Agent.get(__MODULE__, &(&1)) do
       nil -> get_from_server()
-      result -> result
+      {_result, time} when time < now -> get_from_server()
+      {result, _time} -> result
     end
   end
 end
