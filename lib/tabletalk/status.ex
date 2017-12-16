9ca89bb94faf6d288be15434d6947ebf1552a157
strangerelics <castlecryptoid@gmail.com>
2017-12-15 20:11:11 -0800

    Revert "fix takedown location for status"
    This reverts commit ba22ffd3abdc5856b4b4a7d6eb9228bdac00cfff.

--- lib/tabletalk/status.ex
+++ lib/tabletalk/status.ex
@@ -1,13 +1,8 @@
 defmodule Tabletalk.Status do
 
-  def path, do: Application.app_dir(:tabletalk) |> Path.join("../../TAKEDOWN")
-
-  require Logger
-
   def get do
-    Logger.debug path()
-    if File.exists? path() do
-      case File.read path() do
+    if File.exists? "TAKEDOWN" do
+      case File.read "TAKEDOWN" do
         {:ok, binary} -> {:down, binary}
         _else -> {:down, "Tabletalk is experiencing an issue right now. Sorry!"}
       end
@@ -16,5 +11,5 @@ defmodule Tabletalk.Status do
     end
   end
 
-  def down?, do: File.exists? path()
+  def down?, do: File.exists? "TAKEDOWN"
 end
