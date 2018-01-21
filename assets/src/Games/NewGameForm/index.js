aabb57cd933cbcfb8a675a7eb22b6d3effe67329
strangerelics <castlecryptoid@gmail.com>
2018-01-20 20:19:59 -0800

    fix import

--- assets/src/Games/NewGameForm/index.js
+++ assets/src/Games/NewGameForm/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import NewGameForm from './NewGameForm';
 
 import { create } from 'Games/actionCreators';
-import { compose } from '../../../../../../Users/castl/AppData/Local/Microsoft/TypeScript/2.6/node_modules/redux';
+import { compose } from 'redux';
 import withSize from '../../common/withSize';
 
 const mapStateToProps = ({games}) => ({})
