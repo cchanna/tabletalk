5171bf0dccb56f205e15e51566503eacb9ac1759
Clarity Flowers <clarity@strangerelics.com>
2019-02-08 22:38:00 -0800

    Fix build

--- assets/config/paths.js
+++ assets/config/paths.js
@@ -70,6 +70,9 @@ module.exports = {
   dotenv: resolveApp(".env"),
   appPath: resolveApp("."),
   appBuild: resolveApp("build"),
+  appBuildHtml: resolveApp(
+    "../lib/tabletalk_web/templates/page/index.html.eex"
+  ),
   appPublic: resolveApp("public"),
   appHtml: resolveApp("public/index.html"),
   appIndexJs: resolveModule(resolveApp, "src/index"),
