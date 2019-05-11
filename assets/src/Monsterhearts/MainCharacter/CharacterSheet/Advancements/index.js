191fc9f40b024ec2fdfa39b134e7b64c740e36db
Clarity Flowers <clarity@strangerelics.com>
2019-05-11 14:46:46 -0700

    fix bug

--- assets/src/Monsterhearts/MainCharacter/CharacterSheet/Advancements/index.js
+++ assets/src/Monsterhearts/MainCharacter/CharacterSheet/Advancements/index.js
@@ -1,78 +1,96 @@
-import { connect } from 'react-redux'
-import Advancements from './Advancements';
+import { connect } from "react-redux";
+import Advancements from "./Advancements";
 
-import { fromMonsterhearts, forMonsterhearts } from 'state';
-import { goTo } from 'Routing/actionCreators';
-import { getPath } from 'Routing/selectors';
-const { createAdvancement, addStat, cancelAddStat, deleteAdvancement } = forMonsterhearts;
+import { fromMonsterhearts, forMonsterhearts } from "state";
+import { goTo } from "Routing/actionCreators";
+import { getPath } from "Routing/selectors";
+const {
+  createAdvancement,
+  addStat,
+  cancelAddStat,
+  deleteAdvancement
+} = forMonsterhearts;
 
-export const add = ({id, advancementId}) => (dispatch, getState) => {
+export const add = ({ id, advancementId }) => (dispatch, getState) => {
   if (advancementId === "+stat") {
-    dispatch(addStat({id}));
-  }
-  else {
+    dispatch(addStat({ id }));
+  } else {
     if (advancementId === "rtire") {
       const state = getState();
       const { here } = getPath(state, 2);
       dispatch(goTo([...here, "new"]));
     }
-    return dispatch(createAdvancement({id, advancementId}));
+    return dispatch(createAdvancement({ id, advancementId }));
   }
-}
+};
 
-export const remove = ({id, advancementId}) => (dispatch, getState) => {
+export const remove = ({ id, advancementId }) => (dispatch, getState) => {
   const state = getState();
-  const { addingStat } = fromMonsterhearts.getCharacter(state, id).mainCharacter; 
+  const { addingStat } = fromMonsterhearts.getCharacter(
+    state,
+    id
+  ).mainCharacter;
   if (addingStat && advancementId === "+stat") {
-    dispatch(cancelAddStat({id}))
+    dispatch(cancelAddStat({ id }));
+  } else {
+    dispatch(deleteAdvancement({ id, advancementId }));
   }
-  else {
-    dispatch(deleteAdvancement({id, advancementId}))
-  }
-}
+};
 
 const {
-  getCharacter, 
-  getReadOnly, 
-  getPlaybookAdvancements, 
-  listSeasonAdvancements,
+  getCharacter,
+  getReadOnly,
+  getPlaybookAdvancements,
+  getSeasonAdvancements,
   getCanGetSeasonAdvancements
 } = fromMonsterhearts;
 
-const mapStateToProps = (state, {id, depth}) => {
+const mapStateToProps = (state, { id, depth }) => {
   const { mainCharacter } = getCharacter(state, id);
   const readOnly = getReadOnly(state, id);
 
-  const { playbook, addingStat, experience, advancements: selectedAdvancements } = mainCharacter;
-  const advancements = getPlaybookAdvancements(state, playbook)
-    .map(advancement => ({
+  const {
+    playbook,
+    addingStat,
+    experience,
+    advancements: selectedAdvancements
+  } = mainCharacter;
+  const advancements = getPlaybookAdvancements(state, playbook).map(
+    advancement => ({
       ...advancement,
-      selected: advancement.id === "+stat" && !!addingStat 
-    }));
+      selected: advancement.id === "+stat" && !!addingStat
+    })
+  );
 
   let seasonAdvancements = null;
 
   if (getCanGetSeasonAdvancements(state, id)) {
-    seasonAdvancements = listSeasonAdvancements(state)
-      .map(advancement => ({
-        ...advancement,
-        selected: selectedAdvancements.includes(advancement.id)
-      }))
+    seasonAdvancements = getSeasonAdvancements(state).map(advancement => ({
+      ...advancement,
+      selected: selectedAdvancements.includes(advancement.id)
+    }));
   }
 
   selectedAdvancements.forEach(id => {
-    const advancement = advancements.find(a => a.id === id && !a.selected) 
+    const advancement = advancements.find(a => a.id === id && !a.selected);
     if (advancement) {
       advancement.selected = true;
     }
-  })
+  });
   return {
-    id, playbook, readOnly, depth,
-    advancements, seasonAdvancements,
-    canLevel: experience >= 5 && !addingStat,
+    id,
+    playbook,
+    readOnly,
+    depth,
+    advancements,
+    seasonAdvancements,
+    canLevel: experience >= 5 && !addingStat
   };
 };
 
-const mapDispatchToProps = {add, remove}
+const mapDispatchToProps = { add, remove };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Advancements);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Advancements);
